ccb02b73ca88ccf24e1f75ef494887d567429698
pravekumar1 <pravekumar1@gmail.com>
2019-12-07 11:50:52 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
+import {ServerElementComponent} from './server-element/server-element.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CockpitComponent ],
+  declarations: [ AppComponent, HelloComponent, CockpitComponent, ServerElementComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
